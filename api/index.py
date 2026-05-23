fc9acfd73c62d5295bafe644698c4a9fc37c4334
Nishan Thapa <159601043+Mr-nishan@users.noreply.github.com>
2026-05-23 18:14:45 +0545

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,1 @@
-"""Vercel serverless entrypoint for FastAPI."""
-
-import os
-
-from mangum import Mangum
-
 from app.main import app
-from app.services.file_service import ensure_output_dir
-
-os.environ.setdefault("VERCEL", "1")
-ensure_output_dir()
-
-handler = Mangum(app, lifespan="off")
